1e70f6ac83cc07a2f455061a2846ea2fa04dab57
bkhardy <bkhardy@umich.edu>
2024-05-03 11:04:10 -0400

    streamline noise addition code

--- add_noise.py
+++ add_noise.py
@@ -13,10 +13,6 @@ def add_complex_noise(u, v, w, vel_mask, targetSNR, venc):
     v_noise = np.zeros((2 * int(Nx // 2), 2 * int(Ny // 2), 2 * int(Nz // 2)))
     w_noise = np.zeros((2 * int(Nx // 2), 2 * int(Ny // 2), 2 * int(Nz // 2)))
 
-    mag_u = np.zeros((2 * int(Nx // 2), 2 * int(Ny // 2), 2 * int(Nz // 2)))
-    mag_v = np.zeros((2 * int(Nx // 2), 2 * int(Ny // 2), 2 * int(Nz // 2)))
-    mag_w = np.zeros((2 * int(Nx // 2), 2 * int(Ny // 2), 2 * int(Nz // 2)))
-
     targetSNRdb = 10 * np.log10(targetSNR)
 
     mag_multiplier = (
@@ -25,8 +21,8 @@ def add_complex_noise(u, v, w, vel_mask, targetSNR, venc):
     mag_image = vel_mask * mag_multiplier
 
     # DO the downsampling
-    u_noise, mag_u = fft.downsample_phase_img(u, mag_image, venc, 1, targetSNRdb)
-    v_noise, mag_v = fft.downsample_phase_img(v, mag_image, venc, 1, targetSNRdb)
-    w_noise, mag_w = fft.downsample_phase_img(w, mag_image, venc, 1, targetSNRdb)
+    u_noise, _ = fft.downsample_phase_img(u, mag_image, venc, 1, targetSNRdb)
+    v_noise, _ = fft.downsample_phase_img(v, mag_image, venc, 1, targetSNRdb)
+    w_noise, _ = fft.downsample_phase_img(w, mag_image, venc, 1, targetSNRdb)
 
     return u_noise, v_noise, w_noise
